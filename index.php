74003146d84e63e09ab0ae68b31b33898e7bb99a
Frensly <37934443+Frensly@users.noreply.github.com>
2018-05-18 00:15:25 +0000

    Create index.php
    Published

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+/***************************************************************
+ * Welcome to our official site ( viewing source ) version 0.0.1
+****************************************************************/
+-->
+<meta charset="utf-8" />
+<meta id="viewport" name="viewport" content="width=device-width" />
+<title>Project!</title>
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+<link rel="shortcut icon" href="//play.battleserver.net/icos/favicon.ico" />
+<link rel="stylesheet" href="//play.battleserver.net/stylish/main.css" />
+<meta name="apple-mobile-web-app-capable" content="no" />
+
+<!--
+/***************************************************************
+ * The main scripts to load our js files.
+****************************************************************/
+-->
+<script>
+<script src="//play.battleserver.net/js/comingsoon.js"></script>
+</script>
+</body>
+</html>
